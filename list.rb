cd2921938c574e87c3f2f949f07ca0b9bbbd3abf
Elli Gradney <elli@elligradney.io>
2022-01-26 01:10:30 -0600

    add methods to List class

--- list.rb
+++ list.rb
@@ -56,7 +56,81 @@ class List
         @items[0]
 
     end
+
+    def print
+        puts '-------------------------------'
+        puts '          Groceries'
+        puts '-------------------------------'
+        puts 'Index | Item         | Deadline'
+        puts '---------------------'
+        @items.each_with_index { |item, i| puts "#{i}   | #{item.title}       | #{item.deadline}" }
+        puts '---------------------'
+    end
+
+    def print_full_item(index)
+
+        item = @items[index]
+
+        if valid_index?(index)
+            puts " #{item.title}    #{item.deadline}    #{item.description}"
+        end
+
+    end
     
+    def print_priority
+
+        item = @items[0]
+
+        if valid_index?(0)
+            puts " #{item.title}    #{item.deadline}    #{item.description}"
+        end
+
+    end
+
+    def up(index, amount=1)
+
+        if !self.valid_index?(index)
+            return false
+        end
+
+        i = index
+        count = 0
+        
+        while i < self.size-1 && count < amount
+            @items[i], @items[i+1] = @items[i+1], @items[i] 
+            i +=1
+            count += 1
+        end
+
+        true
+        
+    end
+
+
+    def down(index, amount=1)
+
+        if !self.valid_index?(index)
+            return false
+        end
+
+        i = index
+        count = 0
+        
+        while i > 0 && count < amount
+            @items[i], @items[i-1] = @items[i-1], @items[i] 
+            i -=1
+            count += 1
+        end
+
+        true
+        
+    end
+
+    def sort_by_date!
+
+        @items.sort_by! { |item| item.deadline }
+
+    end
 
 
 end
